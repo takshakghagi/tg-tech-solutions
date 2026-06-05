57b2152963801c7e2c1952ceb7d9097b43941335
takshakghagi <takshakghagi@gmail.com>
2026-06-05 17:35:43 +0530

    Fix email transporter syntax

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
   tls: {
     rejectUnauthorized: false  
   }
